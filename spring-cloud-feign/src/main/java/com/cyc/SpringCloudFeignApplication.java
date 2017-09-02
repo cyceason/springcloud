c4fb1743020ea3b3d35a4fe28de220aaa97c3a73
cyc_eason@126.com <cyc_eason@126.com>
2017-09-02 10:17:50 +0800

    init

--- spring-cloud-feign/src/main/java/com/cyc/SpringCloudFeignApplication.java
+++ spring-cloud-feign/src/main/java/com/cyc/SpringCloudFeignApplication.java
@@ -6,14 +6,14 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
- * @EnableFeignClients  ： 启动spring cloud feign功能
+ * @EnableFeignClients ： 启动spring cloud feign功能
  */
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class SpringCloudFeignApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudFeignApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudFeignApplication.class, args);
+    }
 }
