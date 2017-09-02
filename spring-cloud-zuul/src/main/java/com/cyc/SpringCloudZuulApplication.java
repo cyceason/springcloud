5cdc649213b5420445f4faaac0a7f7923d451cc1
cyc_eason@126.com <cyc_eason@126.com>
2017-09-02 11:08:17 +0800

    init

--- spring-cloud-zuul/src/main/java/com/cyc/SpringCloudZuulApplication.java
+++ spring-cloud-zuul/src/main/java/com/cyc/SpringCloudZuulApplication.java
@@ -2,11 +2,13 @@ package com.cyc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class SpringCloudZuulApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudZuulApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudZuulApplication.class, args);
+    }
 }
