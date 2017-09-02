f0641579937e48459cbe0c33d838bdde6c2bf704
cyc_eason@126.com <cyc_eason@126.com>
2017-09-02 10:39:56 +0800

    init

---
+++ spring-sleuth/src/main/java/com/cyc/SpringSleuthApplication.java
@@ -0,0 +1,12 @@
+package com.cyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSleuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSleuthApplication.class, args);
+	}
+}
