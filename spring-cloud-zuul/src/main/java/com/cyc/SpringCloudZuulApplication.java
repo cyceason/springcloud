9382617d68f0c38e8638b62a5142ecc0ff2c76c0
cyc_eason@126.com <cyc_eason@126.com>
2017-09-03 15:40:34 +0800

    init

--- spring-cloud-zuul/src/main/java/com/cyc/SpringCloudZuulApplication.java
+++ spring-cloud-zuul/src/main/java/com/cyc/SpringCloudZuulApplication.java
@@ -3,6 +3,8 @@ package com.cyc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -10,5 +12,15 @@ public class SpringCloudZuulApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudZuulApplication.class, args);
+    }
+
+    /**
+     * 通过正则表达，自定义服务与路由映射的生成关系，如果没有匹配到，则还是会以默认的路由映射规则
+     *
+     * @return
+     */
+    @Bean
+    public PatternServiceRouteMapper serviceRouteMapper() {
+        return new PatternServiceRouteMapper("(?<name>^.+)-(?<version>v.+$)", "${version}/${name}");
     }
 }
