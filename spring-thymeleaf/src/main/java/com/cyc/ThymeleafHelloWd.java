7deeeb78bc6f9f2d7af6d7f3efd706aee8bfdabb
cyc_eason@126.com <cyc_eason@126.com>
2017-09-24 23:43:08 +0800

    init

--- spring-thymeleaf/src/main/java/com/cyc/ThymeleafHelloWd.java
+++ spring-thymeleaf/src/main/java/com/cyc/ThymeleafHelloWd.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
+ * Thymeleaf 自动化配置 ： ThymeleafProperties
  * Created by cyc_e on 2017/9/24.
  */
 @Controller
