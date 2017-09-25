815a098d3fa4d28c1fa5947e5141dd58fedd04d6
cyc_eason@126.com <cyc_eason@126.com>
2017-09-25 22:19:29 +0800

    init

--- spring-redis/src/main/java/com/cyc/SpringRedisApplication.java
+++ spring-redis/src/main/java/com/cyc/SpringRedisApplication.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -17,11 +18,17 @@ public class SpringRedisApplication {
         SpringApplication.run(SpringRedisApplication.class, args);
     }
 
-
+    /**
+     * spring boot自动配置了RedisTemplate，RedisTemplate使用了JdkSerializationRedisSerializer，这个使用二进制形式存储数据，不直观
+     * 因此这里自己配置RedisTemplate并定义序列化
+     *
+     * @param redisConnectionFactory
+     * @return
+     */
+    @Bean
     public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
-
         Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
         ObjectMapper om = new ObjectMapper();
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
